d18932cc62fa69df02845a32aaca680221e8ddee
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2026-03-27 15:47:46 -0400

    Added

--- final_experiments/aces_train_multi_node_ulysses.sh
+++ final_experiments/aces_train_multi_node_ulysses.sh
@@ -29,8 +29,8 @@ LAUNCHER="torchrun \
 
 CMD="train_gpt_ulysses.py --seq_length ${SEQUENCE_LENGTH}"
 export HF_TOKEN=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
-export TRANSFORMERS_OFFLINE=1
-export HF_DATASETS_OFFLINE=1
+# export TRANSFORMERS_OFFLINE=1
+# export HF_DATASETS_OFFLINE=1
 mkdir -p aces_default_exp
 srun --export=ALL -l bash -c "${LAUNCHER} ${CMD}" > "aces_default_exp/default_run_${SEQUENCE_LENGTH}.log" 2>&1
 # deepspeed --num_gpus 4 train_gpt_grouped.py
