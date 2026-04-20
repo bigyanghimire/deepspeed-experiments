349d1f15289b6d67e51309bc61a909b03ac5c726
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2026-04-20 16:22:35 -0400

    updated dataloader

--- final_experiments/aces_final_experiments/dataload_test.py
+++ final_experiments/aces_final_experiments/dataload_test.py
@@ -1,24 +1,29 @@
 from datasets import load_dataset
+import sys
+import os
+
+project_dir = sys.argv[1] if len(sys.argv) > 1 else "./"
+dataset_cache_path = os.path.join(project_dir, '.cache/huggingface/datasets')
 
 dataset = load_dataset(
     'wikitext',
     'wikitext-103-raw-v1',
     split='train[:5%]',
-    cache_dir='/scratch/user/u.bg348806/.cache/huggingface/datasets'
+    cache_dir=dataset_cache_path
 )
 
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 model_name_or_path = "meta-llama/Llama-3.2-1B"
-cache_path = "/scratch/user/u.bg348806/.cache/huggingface/hub"
+hub_cache_path = os.path.join(project_dir, '.cache/huggingface/hub')
 
 tokenizer = AutoTokenizer.from_pretrained(
     model_name_or_path,
-    cache_dir=cache_path
+    cache_dir=hub_cache_path
 )
 
 model = AutoModelForCausalLM.from_pretrained(
     model_name_or_path,
-    cache_dir=cache_path
+    cache_dir=hub_cache_path
 )
