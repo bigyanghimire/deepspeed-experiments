d14a315056d90bd9284802dc8e427049ba128db2
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2026-04-28 15:52:13 -0400

    Added

--- final_experiments/aces_final_experiments/aces_train_model.sh
+++ final_experiments/aces_final_experiments/aces_train_model.sh
@@ -14,7 +14,7 @@ PROJECT_DIR="$SCRATCH/bigyan_project"
 MICROMAMBA_DIR="$PROJECT_DIR/.local/bin"
 eval "$("$MICROMAMBA_DIR/micromamba" shell hook -s bash)"
 eval "$(micromamba shell hook --shell bash)"
-micromamba activate ds-hf 
+micromamba activate ds-hf2 
 
 export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
 export MASTER_PORT=29501
@@ -24,16 +24,18 @@ set +a
 export TRANSFORMERS_OFFLINE=1
 export HF_DATASETS_OFFLINE=1
 export HF_HUB_OFFLINE=1
-export HF_HOME="/scratch/user/u.bg348806/.cache/huggingface/hub"
-export HF_HUB_CACHE="/scratch/user/u.bg348806/.cache/huggingface/hub"
-export HF_DATASETS_CACHE="/scratch/user/u.bg348806/.cache/huggingface/datasets"
+export HF_HOME="${PROJECT_DIR}/.cache/huggingface/hub"
+export HF_HUB_CACHE="${PROJECT_DIR}/.cache/huggingface/hub"
+export HF_DATASETS_CACHE="${PROJECT_DIR}/.cache/huggingface/datasets"
 export PATH="${PATH}"
 
 NUM_NODES=2
-GPUS_PER_NODE=1
-SEQUENCE_LENGTH=256
+GPUS_PER_NODE=4
+SEQUENCE_LENGTH=${SEQUENCE_LENGTH:-32768}
 SEQ_PARALLEL_SIZE=$((NUM_NODES * GPUS_PER_NODE))
-
+BATCH_SIZE=2
+MODEL_NAME="meta-llama/Llama-3.2-3B"
+MODEL_NAME_SHORT="llama3B"
 MODE=$1  # Accept 'grouped' or 'ulysses' as first argument
 
 # --- Logic Gate ---
@@ -48,9 +50,10 @@ else
     exit 1
 fi
 
-mkdir -p $LOG_DIR
+mkdir -p "${LOG_DIR}/${MODEL_NAME_SHORT}"
 mkdir -p slurm_logs
 
+# --- Execution ---
 LAUNCHER="torchrun \
     --nproc_per_node ${GPUS_PER_NODE} \
     --nnodes ${NUM_NODES} \
@@ -58,11 +61,8 @@ LAUNCHER="torchrun \
     --rdzv_backend=c10d \
     --rdzv_endpoint=${MASTER_ADDR}:${MASTER_PORT}"
 
-
-CMD="${SCRIPT_NAME} --seq_length ${SEQUENCE_LENGTH} --seq_parallel_size ${SEQ_PARALLEL_SIZE} --type ${MODE}"
+CMD="${SCRIPT_NAME} --seq_length ${SEQUENCE_LENGTH} --seq_parallel_size ${SEQ_PARALLEL_SIZE} --type ${MODE} --batch_size ${BATCH_SIZE} --model_name ${MODEL_NAME}"
 
 echo "Running in ${MODE} mode using ${SCRIPT_NAME}..."
 
-srun --export=ALL -l bash -c "${LAUNCHER} ${CMD}" > "${LOG_DIR}/${MODE}_run_${SEQUENCE_LENGTH}.log" 2>&1
-# deepspeed --num_gpus 4 train_gpt_grouped.py
-
+srun --export=ALL -l bash -c "${LAUNCHER} ${CMD}" > "${LOG_DIR}/${MODEL_NAME_SHORT}/${MODE}_${SEQUENCE_LENGTH}_batch_${BATCH_SIZE}.log" 2>&1
