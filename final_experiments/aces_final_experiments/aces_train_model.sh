05bdbaceef05daae6a2f0db131cf3eafab1ddbec
Bigyan Ghimire <ghimire.vigyan@gmail.com>
2026-04-28 22:10:57 -0400

    Added

--- final_experiments/aces_final_experiments/aces_train_model.sh
+++ final_experiments/aces_final_experiments/aces_train_model.sh
@@ -10,7 +10,8 @@
 module purge
 module load modtree/gpu
 module load cuda/11.8.0
-module load gcc/10.2.0
+gcc --version
+#module load gcc/10.2.0
 PROJECT_DIR="$SCRATCH/bigyan_project"
 MICROMAMBA_DIR="$PROJECT_DIR/.local/bin"
 eval "$("$MICROMAMBA_DIR/micromamba" shell hook -s bash)"
